9dbaf75f0abf563e65def3f3a005741d96f8e679
Daniel Pérez <dpdaniperezsanz@gmail.com>
2025-08-17 15:57:21 +0200

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: mode === 'development' ? '/' : '/apuntes_csee/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
